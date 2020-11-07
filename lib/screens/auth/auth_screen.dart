37ee77788bdaf5fc2126a5fd3fc102bcb1575882
hashiqvh <mohamedhashiq88@gmail.com>
2020-11-07 20:28:05 +0530

    auth login ui

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:padavukal/features/auth/bloc/auth_bloc.dart';
+import 'package:padavukal/widgets/outline_button.dart';
 
 class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: RaisedButton.icon(
-          onPressed: () => BlocProvider.of<AuthBloc>(context).add(LogInEvent()),
-          icon: Icon(Icons.login),
-          label: Text("login"),
+        body: SafeArea(
+            child: Column(
+      children: [
+        SizedBox(height: deviceSize.height * 0.2),
+        Image.asset(
+          "assets/images/padavukal 1.png",
+          scale: 1,
         ),
-      ),
-    );
+        SizedBox(height: deviceSize.height * 0.05),
+        Center(
+          child: OutLinedButton(
+            title: "Sign in with Google",
+            imageUrl: "assets/images/googleIcon.png",
+            width: deviceSize.width * 0.234,
+          ),
+        ),
+      ],
+    )));
   }
 }
---
+++ lib/widgets/outline_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:padavukal/features/auth/bloc/auth_bloc.dart';
+
+class OutLinedButton extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final double width;
+
+  OutLinedButton({this.title, this.imageUrl, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      child: OutlineButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(5.0)),
+        // padding: EdgeInsets.all(10),
+        onPressed: () => BlocProvider.of<AuthBloc>(context).add(LogInEvent()),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Image.asset(
+                imageUrl,
+                height: 20,
+                width: 20,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Text(
+                title,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        ),
+        borderSide: BorderSide(
+          color: Colors.blue,
+          style: BorderStyle.solid,
+          width: 0.8,
+        ),
+      ),
+    );
+  }
+}
