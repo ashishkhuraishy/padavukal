d3d005db58970fc0d5d24b8576f2040054869b97
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-11-08 14:38:39 +0530

    updated auth

--- lib/widgets/outline_button.dart
+++ lib/widgets/outline_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:padavukal/features/auth/bloc/auth_bloc.dart';
+
+import '../features/auth/presentation/bloc/auth_bloc.dart';
 
 class OutLinedButton extends StatelessWidget {
   final String title;
